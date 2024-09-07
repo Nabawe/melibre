0196218cdd0e1471146c81c3f70056f06e5bde19
Nabawe <Nabawe@users.noreply.github.com>
2024-09-07 02:03:40 -0300

    working on RMABox.ts comments used comments.ts as a temp file

--- src/classes/comments.ts
+++ src/classes/comments.ts
@@ -1,10 +1,26 @@
-/* WIP
-    Error de diceño fundamental, no se deberia dar acceso directo a this.i ( items ), sino q todo se haga a travez de metodos para garantizar integridad.
+/* ? Needs polishing, fundaments or criteria
+    + No se deberia dar acceso directo a this.i ( items ), sino q todo se haga a travez de metodos para garantizar integridad.
 
-    Operaciones Sincronicas a Asincronicas:
+    + // ! RAMBox should NOT let to manually specify or MODIFY IDs, IDs MUST BE READ ONLY or only modifiable internally by the class to restructure, reindex, etc. At most what a user can do is to issue a delete and recreate the item.
+
+    + // ! Many aspects of the class struggle to define a general form, I believe the class should manage the simplests aspects of the Item's Array. The incoming data should be checked by a specialized part of the server as soon as it is received from the front. This class instead should focus on the integrity of the data so any external customization SHOULD BE AVOIDED BY DESIGN.
+    Origins of this thought came from thinking that #init, m_new, m_set would need an external specification of the Item's Extra Properties and how to handle, verify, compare them, etc.
+    The other way to think about this is that the class should be re-written for each use case.
+*/
+/* WIP
+    + Operaciones Sincronicas a Asincronicas
+    + #init
+    + #dataChecks
+
+
+    + Operaciones Sincronicas a Asincronicas:
+        // * Lo q se tiene q lograr es q haya capas de cache en RAM y tmp files para garantizar q no se pierda información y q solo se graben los datos de forma paralela cuando se pueda efectuar la totalidad de la operación.
+        // * O sea definir q hace cada capa, como y cuando. ( Volatile, Small Tmp File, Big Storage ).
+        // * Prestar mucha atención q es resuelto automaticamente por las herramientas q ya existen, ej: por ahi alguna base de datos ya tiene algun tipo de cache.
+
         Quiero q m_fileSave, m_fileReset y #init sean async:
             - #init:
-                · Posiblemente tenga q comunicar q se están cargando los datos.
+                · Posiblemente tenga q comunicar q se están cargando los datos. Pero la initialización del servidor no debería ser async.
 
             - m_fileSave:
                 1 - Retorna aviso de q esta por comensar para q tanto el Backend como el Frontend bloqueen todas las operaciones q usen el modulo fs ( los metodos q comienzan con m_file ) pero NO las que se realizen solo en RAM ( las q manipulan this.i ), colocar los botones en estado disabled, etc.
@@ -17,99 +33,20 @@
             - m_fileReset:
                 1 - Corre m_reset.
                 Idem del punto 1 a 4 de m_fileSave
+    +
 
+    + #init
+        - Hacer q cree el archivo si no existe o q eso pase al apretar save (commit to file)? (Por ahora si pasara eso habria un error al pasar el arg o se dispararia el catch); también tendría q crear los directorios.
+        - // ! Esto puede tambien fallar si la totalidad del archivo es uno de los tipos minimos de JSON ejemplo si solo tuviera null dentro. No se si es suficiente el checkear el lenght.
+        - // ! Aqui return new Error no tiene sentido ya q no hay nadie q lo capture al error y lo muestre.
+        - // ! No tiene sentido q sea un JSON, agrega un parse q no existiria si fuera directamente un objeto exportado de un file.ts .
+    +
+
+    + #dataChecks
+        - Cuanto sentido tiene tener la posibilidad de agregar flags, no esta hecho a la medida? Y sino no tendria q ser uno de los parametros q initializa la clase?
+            // * O sea lo de agregar nuevas flags tendria q pasar a nivel development y no runtime por ende no tiene sentido complicarlo. Y pensar q los checkeos tienen q ser algo fundamental q se aplique a situaciones generales y no a un tipo especifico de Items.
+    +
 */
-
-
-/* TO-DO Add this quirk to DocString No permite actualizar el id de forma manual, de necesitarlo borrar el producto y re agregarlo, así garantiza el uso correcto de UUID */
-/** Creates a simple interface wich helps manipulate a basic array of items stored in a JSON file. A JSON file's internal items manager. This variation of JSONBox makes its instances work on a RAM cached array and they only commit it to JSON file on command. */
-
-
-// WIP Hacer q esto tambien sea ASYNC
-    /* WIP hacer q cree el archivo si no existe o q eso pase al apretar save (commit to file)? (Por ahora si pasara eso habria un error al pasar el arg o se dispararia el catch); también tendría q crear los directorios */
-        /* * Ver cual es el resutlado del error para q el catch lo haga, o sea si JSON.parse o readFile dan error ver q se le pasa al catch por err y ejecutar solución */
-    /* WIP MISSING PARAMETHERS
-        as Objects
-            - Items Extra Properties might need an object carrying functions that set how to compare and operate those new properties, for now it will use simple comparison.
-            - #dataChecks, DataChecksFlags
-            ! Complete the ErrsMsgs errors to adapt to new values given to dataChecks.
-            - The Init method might probably need the form of the base item.
-
-        ! All this remarks make one think that the Class Should be re-written for each use case.
-    */
-    /* ! Esto puede tambien fallar si la totalidad del archivo es uno de los tipos minimos de JSON ejemplo si solo tuviera null dentro. No se si es suficiente el checkear el lenght */
-    /* ! Aqui return new Error no tiene sentido ya q no hay nadie q lo capture al error y lo muestre */
-    /* ! No tiene sentido q sea un JSON, agrega un parse q no existiria si fuera directamente un objeto exportado de un file.ts . */
-//    #init(): false | Error | void {
-
-
-
-    // WIP Add Other Checks
-    /* WIP Add ways to specify the checks like with a hash 3b40v69 or binary string 010110 or flags object, so it can skip unnecesary checks for a given scenario */
-    // * Both Parameters are optional
-    // ! this function will NEVER return void, always false or a specific Error w a cause
-// #dataChecks( flags?: DataChecksFlags | undefined, data = this.i ): false | Error | void {
-        // Add new default values to flags in F.
-        /*
-            let F: DataChecksFlags = { NO_DATA: true, ...flags };
-            This line should fail when flags is undefined or an empty object since both are not iterables
-        */
-// ! Pero cuanto sentido tiene tener la posibilidad de agregar cosas , no esta hecho a la medida? Y sino no tendria q ser uno de los parametros q initializa la clase?
-
-
-
-
-    // TO-DO DO A METHOD to search by any property, the way to evaluate each property might be needed
-        // m_getById( id: t_Item["id"] ): t_Item | Error {
-
-
-
-
-    //  m_new( newItem: t_Item ) {
-        // ! HERE BEFORE SENDING req.body there should be somekind of check that confirm the integrity of the data from the front
-            // O sea q o products route checkea o datachecks
-            // !!! PERO si datachecks lo tiene q hacer entonces se le tendria q pasar para adaptarse a cada caso!!
-            // * dataChecks seria solo para cosas como existencia de datos, verificar q haya donde guardarlos y otras generales pero en routes iria el revisar la integridad q el front le paso
-
-
-
-
-  // Se podria usar delete[index] y luego al grabar o reindexar remover los undefined
-//   m_del( id: t_IndexKey ) {
-
-
-
-
-
-// ! m_set is subject to the same checking problems as m_new
-// m_set( id: t_IndexKey, data: t_Item ){
-    /* The idea was if the ID did not exist it would create a new item, but that kind of behaviour may lead to create entries by mistake when mistyping an Id. */
-            // return this.m_new( data ); // ! Se corre dataChecks 2 veces así
-
-
-
-
-
-
-
-
-
-    /*
-        ! No crea un archivo nuevo si no existe, de tener q crearlo tambien tendria q poder crear toda la ruta, o sea los dirs
-
-        ! Tendria q usar append y es una animalada hacerlo de esa forma, esta en las preguntas Questons-02.txt.
-
-        ? confirmar q el metodo append de FS no cargue todo el archivo para modificarlo
-    */
-        // m_fileSave() {
-
-
-
-
-
-
-
-
-
-    // Pensar ¿Tendria algun sentido q solo borre el archivo del disco pero deje sin tocar la RAM?
-    // m_fileReset() {
+/* TO-DO
+    + DO A METHOD to search by any property, the way to evaluate each property might be needed or use simple comparisons, search like exact and lazy, fuzy.
+*/
